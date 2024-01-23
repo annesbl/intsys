82081bbbfb4317b70978af5fbe00e57927798464
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2024-01-23 10:36:28 +0100

    etwas funktioniert!!!

--- anton_python_stuff.py
+++ anton_python_stuff.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
 directory = "messdaten/"
 
@@ -18,21 +19,26 @@ def transform_data(df):
          > d.h. es gibt 305 - 61 = 244 output zeilen im df
     """
     # transpose from (n, 1) -> (1, n)
-    df_transposed = df.T
+    df_transposed = df
     output_data = []
 
     for i in range(244): # 244 because 305 - 61 (letzte Umdrehung wird rausgenommen, damit jeder neue teil eine komplette umdrehung hat)
-        one_turn = df_transposed.iloc[:, i:i+61]
-        print(one_turn)
-        # output_data.append(one_turn)
-     
-    output_df = pd.DataFrame(output_data)
-
-    return output_df
-
+        one_turn = df_transposed.iloc[i:i+61, :]
+        # print(one_turn)
+        output_data.append(one_turn)
+    dfs_reset_index = [df.reset_index(drop=True) for df in output_data]
+    result_df = pd.concat(dfs_reset_index, axis=1)
+    return result_df
 
 
 df_new = transform_data(df)
 
 # print(df)
-print(df_new)
+# print(df_new)
+
+
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+angles = np.linspace(0, 2 * np.pi, 61, endpoint=False)
+ax.plot(angles, df_new.iloc[:, 0])
+plt.show()
